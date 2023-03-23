47b252b85ce2f8e8dbd249fbc2fa377c3a3460dc
TadaDaisuke <td2000@ad.wakwak.com>
2023-03-23 23:53:12 +0900

    開発中

--- MyAspDotNet6App/SqlDataAccess/MyAppContext.cs
+++ MyAspDotNet6App/SqlDataAccess/MyAppContext.cs
@@ -13,7 +13,7 @@ namespace MyAspDotNet6App.SqlDataAccess
             _connectionString = connectionString;
         }
 
-        public List<Dictionary<string, string?>> GetRowList(SqlCommand cmd, List<string>? fieldNames = null, List<SqlDbType>? fieldTypes = null)
+        public List<Dictionary<string, string?>> GetRowList(SqlCommand cmd)
         {
             var list = new List<Dictionary<string, string?>>();
             using (var conn = new SqlConnection(_connectionString))
@@ -22,30 +22,21 @@ namespace MyAspDotNet6App.SqlDataAccess
                 cmd.Connection = conn;
                 using (var reader = cmd.ExecuteReader(CommandBehavior.SequentialAccess | CommandBehavior.SingleResult | CommandBehavior.CloseConnection))
                 {
-                    if (fieldNames != null)
+                    var uniqueColNames = new List<string>();
+                    for (var index = 0; index < reader.FieldCount; index++)
                     {
-                        for (var index = 0; index < reader.FieldCount; index++)
-                        {
-                            fieldNames.Add(reader.GetName(index));
-                        }
-                    }
-                    if (fieldTypes != null)
-                    {
-                        for (var index = 0; index < reader.FieldCount; index++)
-                        {
-                            fieldTypes.Add((SqlDbType)Enum.Parse(typeof(SqlDbType), reader.GetDataTypeName(index), true));
-                        }
+                        var colName = reader.GetName(index);
+                        uniqueColNames.Add((string.IsNullOrWhiteSpace(colName) || uniqueColNames.Contains(colName)) ? Guid.NewGuid().ToString() : colName);
                     }
                     while (reader.Read())
                     {
                         var dict = new Dictionary<string, string?>(reader.FieldCount);
                         for (var index = 0; index < reader.FieldCount; index++)
                         {
-                            var colName = reader.GetName(index);
                             var colValue = reader.GetValue(index);
                             dict.Add
                             (
-                                string.IsNullOrWhiteSpace(colName) ? Guid.NewGuid().ToString() : colName,
+                                uniqueColNames[index],
                                 (colValue == null || colValue == DBNull.Value)
                                     ? null
                                     : colValue is DateTime dateTimeValue
@@ -59,7 +50,5 @@ namespace MyAspDotNet6App.SqlDataAccess
             }
             return list;
         }
-
-
     }
 }
